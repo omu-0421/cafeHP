0a327ba3fa6476ae7d2d18f90cbefad691bb8e13
omu-0421 <64574117+omu-0421@users.noreply.github.com>
2020-06-30 09:41:19 +0900

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('DOMContentLoaded', function() {
-    var swiper = new Swiper('.sample01 .swiper-container', {
-      pagination: '.swiper-pagination',
-      paginationClickable: true,
-      nextButton: '.swiper-button-next',
-      prevButton: '.swiper-button-prev',
-      loop: true
-    });
-  }, false);
